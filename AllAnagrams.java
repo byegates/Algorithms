e1ae90de9cf4f23384279a3a1cd00fb45ce1b067
Weilong <byegates@gmail.com>
2022-03-13 18:54:50 -0400

    Make sure toMatch is updated correctly every step (although the old solution works perfectly fine) make the code easier to understand

--- AllAnagrams.java
+++ AllAnagrams.java
@@ -8,10 +8,7 @@
 //
 //        l = "abcbac", s = "ab", return [0, 3] since the substring with length 2 starting from index 0/3 are all anagrams of "ab" ("ab", "ba").
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AllAnagrams {
     public static List<Integer> allAnagrams(String s, String l) {
@@ -21,25 +18,38 @@ public class AllAnagrams {
         for (int i = 0; i < s.length(); i++)
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
 
-        int charsToMatch = map.size();
+        int toMatch = map.size();
         for (int j = 0; j < l.length(); j++) {
-            if (updateCount(l.charAt(j), -1, map) == 0) charsToMatch--;
-            if (j >= s.length() && updateCount(l.charAt(j - s.length()), 1, map) == 1) charsToMatch++;
-            if (charsToMatch == 0) res.add(j - s.length() + 1);
+            toMatch = updateMap(-1, l.charAt(j), toMatch, map);
+            if (j >= s.length()) toMatch = updateMap(1, l.charAt(j - s.length()), toMatch, map);
+            if (toMatch == 0) res.add(j - s.length() + 1); // j - s.length() is what we just got rid of, so next index is the start of the keeper substring
         }
 
         return res;
     }
 
-    private static int updateCount(char ch, int val, Map<Character, Integer> map) {
-        int count = map.getOrDefault(ch, Integer.MIN_VALUE);
-        if (count != Integer.MIN_VALUE) map.put(ch, count += val);
-        return count;
+    private static int updateMap(int val, char ch, int toMatch, Map<Character, Integer> map) {
+        Integer count = map.get(ch);
+        if (count == null) return toMatch;
+        if (count == 0) toMatch++;
+        if (count == 1 && val == -1 || count == -1 && val == 1) toMatch--;
+        map.put(ch, count + val);
+        return toMatch;
     } // TC: O(m + n), SC: O(m), m is the length of s and n is the length of l
 
+    private static List<Integer> toList(int[] A) {
+        List<Integer> res = new ArrayList<>(A.length);
+        for (int i : A) res.add(i);
+        return res;
+    }
+
     public static void main(String[] args) {
-        System.out.println(allAnagrams("ab", "abcbac"));
-        System.out.println(allAnagrams("aab", "ababacbbaac"));
+        List<Integer> res1 = allAnagrams("ab", "abcbac");
+        System.out.println(res1);
+        System.out.println("Is above result correct ? " + res1.equals(toList(new int[]{0, 3})));
+        List<Integer> res2 = allAnagrams("aab", "ababacbbaac");
+        System.out.println(res2);
+        System.out.println("Is above result correct ? " + res2.equals(toList(new int[]{0, 2, 7})));
     }
 }
 
