53701cb41ba528cef560e70c5c2f0ce77d75a35c
Weilong <byegates@gmail.com>
2022-03-16 01:13:56 -0400

    Minor code change to improve performance

--- EditDistance.java
+++ EditDistance.java
@@ -39,7 +39,7 @@ public class EditDistance {
 
         int[][] dp = new int[len1 + 1][len2 + 1];
         for (int i = 0; i <= len1; i++) dp[i][0] = i;
-        for (int j = 0; j <= len2; j++) dp[0][j] = j;
+        for (int j = 1; j <= len2; j++) dp[0][j] = j;
 
         for (int i = 1; i <= len1; i++)
             for (int j = 1; j <= len2; j++)
