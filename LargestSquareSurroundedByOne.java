fce90743b50329de210cec54b81d5c18b444c678
Weilong <byegates@gmail.com>
2022-03-18 04:40:30 -0400

    Added method 2 which do not use extra spaces(row & col)

--- LargestSquareSurroundedByOne.java
+++ LargestSquareSurroundedByOne.java
@@ -38,6 +38,30 @@ public class LargestSquareSurroundedByOne {
         return max;
     }
 
+    // Solution 2, normal-sized M1 and M2, no extra row & col
+    public int noExtraRowCol(int[][] mx) {
+        if (mx.length == 0 || mx[0].length == 0) return 0;
+        int n = mx.length, m = mx[0].length;
+
+        int[][] M1 = new int[n][m]; // right to left, extra col & row to avoid boundary check in main loop
+        int[][] M2 = new int[n][m]; //
+
+        int max = mx[0][0];
+        for (int i = n - 1; i >= 0; i--) // starting from 2nd bottom row to top
+            for (int j = m - 1; j >= 0; j--) {// starting from 2nd right row to left
+                M1[i][j] = mx[i][j] == 0 ? 0 : (j + 1 >= m ? 0 : M1[i][j + 1]) + 1;
+                M2[i][j] = mx[i][j] == 0 ? 0 : (i + 1 >= n ? 0 : M2[i + 1][j]) + 1;
+                // as we are filling M1 & M2 from bottom & right, we can/must iterate i,j as top left vertices of square
+                for (int maxLen = Math.min(M1[i][j], M2[i][j]); maxLen > 0; maxLen--)
+                    if (M1[i + maxLen - 1][j] >= maxLen && M2[i][j + maxLen - 1] >= maxLen) {
+                        if (maxLen > max) max = maxLen;
+                        break;
+                    }
+            }
+
+        return max;
+    }
+
     public static void main(String[] args) {
         int[][] mx = new int[][] {
                 {1,1,1,1,1,1,0,1,1,1,0,0,1,1,1,0,0,1},
@@ -62,5 +86,7 @@ public class LargestSquareSurroundedByOne {
         LargestSquareSurroundedByOne lss = new LargestSquareSurroundedByOne();
         System.out.println(lss.largestSquareSurroundedByOne(mx)); // 5
         System.out.println(lss.largestSquareSurroundedByOne(mx2)); // 3
+        System.out.println(lss.noExtraRowCol(mx)); // 5
+        System.out.println(lss.noExtraRowCol(mx2)); // 3
     }
 }
