d9da17eee34bd063ec6215e21603d267bc59198e
Weilong <byegates@gmail.com>
2022-03-18 03:20:02 -0400

    change to block comments

--- LongestCrossOf1s.java
+++ LongestCrossOf1s.java
@@ -1,18 +1,19 @@
-//Given a matrix that contains only 1s and 0s, find the largest cross which contains only 1s,
-// with the same arm lengths and the four arms joining at the central point.
-//
-//        Return the arm length of the largest cross.
-//
-//        Assumptions
-//        The given matrix is not null, has size of N * M, N >= 0 and M >= 0.
-//        Examples
-//
-//        { {0, 0, 0, 0},
-//          {1, 1, 1, 1},
-//          {0, 1, 1, 1},
-//          {1, 0, 1, 1} }
-//
-//        the largest cross of 1s has arm length 2.
+/*      Given a matrix that contains only 1s and 0s, find the largest cross which contains only 1s,
+        with the same arm lengths and the four arms joining at the central point.
+
+        Return the arm length of the largest cross.
+
+        Assumptions
+        The given matrix is not null, has size of N * M, N >= 0 and M >= 0.
+        Examples
+
+        { {0, 0, 0, 0},
+          {1, 1, 1, 1},
+          {0, 1, 1, 1},
+          {1, 0, 1, 1} }
+
+        the largest cross of 1s has arm length 2.
+*/
 
 public class LongestCrossOf1s {
     public int largest(int[][] mx) { // TC: 5*n*m--> O(n*m), SC: 4*n*m --> O(n*m)
