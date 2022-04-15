e741848e022b3462e16847ca01d73a6ad0b5585d
Weilong <byegates@gmail.com>
2022-04-14 22:43:16 -0400

    Simplify code

--- KthClosestPointTo000.java
+++ KthClosestPointTo000.java
@@ -25,43 +25,44 @@ import java.util.*;
 public class KthClosestPointTo000 {
     public List<Integer> closest(int[] a, int[] b, int[] c, int k) { // TC: O(klogk), SC: O(k)
         List<Integer> res = new ArrayList<>();
-        PriorityQueue<List<Integer>> pq = new PriorityQueue<>(2 * k, new Comparator<List<Integer>>() {
-            @Override
-            public int compare(List<Integer> o1, List<Integer> o2) {
-                long d1 = distance(o1, a, b, c);
-                long d2 = distance(o2, a, b, c);
-                return Long.compare(d1, d2);
-            }
-        });
+//        PriorityQueue<List<Integer>> pq = new PriorityQueue<>(2 * k, new Comparator<List<Integer>>() {
+//            @Override
+//            public int compare(List<Integer> o1, List<Integer> o2) {
+//                long d1 = distance(o1, a, b, c);
+//                long d2 = distance(o2, a, b, c);
+//                return Long.compare(d1, d2);
+//            }
+//        }); // use above Comparator if lambda is not supported
+        // we poll 1 and add max 3 (with dup) each time, so total pq size will be < 2 * k
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>(2 * k, Comparator.comparingLong(o -> distance(o, a, b, c)));
 
-        Set<List<Integer>> visited = new HashSet<>();
+        Set<List<Integer>> set = new HashSet<>();
         List<Integer> cur = Arrays.asList(0, 0, 0);
-        visited.add(cur);
+        set.add(cur);
         pq.offer(cur);
 
-        while (k-- > 0) {
+        while (k-- > 0) { // we added 0,0,0 as start then polled k points out(effectively we polled k - 1), so top of heap will be the kth smallest
             cur = pq.poll();
-            List<Integer> next = Arrays.asList(cur.get(0) + 1, cur.get(1), cur.get(2));
-            if (next.get(0) < a.length && visited.add(next)) pq.offer(next);
-            next = Arrays.asList(cur.get(0), cur.get(1) + 1, cur.get(2));
-            if (next.get(1) < b.length && visited.add(next)) pq.offer(next);
-            next = Arrays.asList(cur.get(0), cur.get(1), cur.get(2) + 1);
-            if (next.get(2) < c.length && visited.add(next)) pq.offer(next);
+            if (cur.get(0) + 1 < a.length) generate(cur, 1, 0, 0, set, pq);
+            if (cur.get(1) + 1 < b.length) generate(cur, 0, 1, 0, set, pq);
+            if (cur.get(2) + 1 < c.length) generate(cur, 0, 0, 1, set, pq);
         }
 
-        cur.set(0, a[cur.get(0)]);
-        cur.set(1, b[cur.get(1)]);
-        cur.set(2, c[cur.get(2)]);
+        return Arrays.asList(a[cur.get(0)], b[cur.get(1)], c[cur.get(2)]);
+    }
 
-        return cur;
+    private void generate(List<Integer> cur, int a, int b, int c, Set<List<Integer>> set, PriorityQueue<List<Integer>> pq) {
+        List<Integer> next = Arrays.asList(cur.get(0) + a, cur.get(1) + b, cur.get(2) + c);
+        if (!set.add(next)) return;
+        pq.offer(next);
     }
 
     private long distance(List<Integer> point, int[]a, int[] b, int[] c) {
-        long dis = 0;
-        dis += (long) a[point.get(0)] * a[point.get(0)];
-        dis += (long) b[point.get(1)] * b[point.get(1)];
-        dis += (long) c[point.get(2)] * c[point.get(2)];
-        return dis;
+        return pow2(a[point.get(0)]) + pow2(b[point.get(1)]) + pow2(c[point.get(2)]);
+    }
+
+    private long pow2(int v) {
+        return (long) v * v;
     }
 
     public static void main(String[] args) {
