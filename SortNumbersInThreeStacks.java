3986cdb0415ca7e307002f3ec35aa3ba12797b6d
Weilong <byegates@gmail.com>
2022-04-22 00:54:34 -0400

    More comments

--- SortNumbersInThreeStacks.java
+++ SortNumbersInThreeStacks.java
@@ -13,10 +13,10 @@
 import java.util.LinkedList;
 
 public class SortNumbersInThreeStacks {
-    public void sort(LinkedList<Integer> s1) {
+    public void sort(LinkedList<Integer> s1) { // 2.5*nlogn ==> O(nlogn)
         LinkedList<Integer> s2 = new LinkedList<>();
         LinkedList<Integer> s3 = new LinkedList<>();
-        sort(s1, s2, s3, s1.size());
+        sort(s1, s2, s3, s1.size()); // sort top s1.size() values in s1 in ascending order from top to bottom
     }
 
     private void sort(LinkedList<Integer> s1, LinkedList<Integer> s2, LinkedList<Integer> s3, int len) {
@@ -26,12 +26,15 @@ public class SortNumbersInThreeStacks {
         for (int i = 0; i < mid1; i++)
             s2.offerFirst(s1.pollFirst());
 
-        sort(s2, s3, s1, mid1);
-        sort(s1, s3, s2, mid2);
+        // the key is to sort s2 and s1, the sequence of 2nd and 3rd stack doesn't matter
+        // sort(s2, s3, s1, mid1); will also work
+        // sort(s1, s3, s2, mid2); will also work
+        sort(s2, s1, s3, mid1); // sort top mid1 values in s2 in ascending order from top to bottom
+        sort(s1, s2, s3, mid2);// sort top mid2 values in s1 in ascending order from top to bottom
 
         int i = 0, j = 0;
 
-        while (i < mid1 && j < mid2 && !s1.isEmpty() && !s2.isEmpty()) // s1 and s2 will not be empty, added only to avoid IDE warning
+        while (i < mid1 && j < mid2)
             if (s2.peekFirst() < s1.peekFirst()) {
                 s3.offerFirst(s2.pollFirst());
                 i++;
@@ -40,8 +43,8 @@ public class SortNumbersInThreeStacks {
                 j++;
             }
 
-        while (i++ < mid1) s3.offerFirst(s2.pollFirst());
-        while (j++ < mid2) s3.offerFirst(s1.pollFirst());
+        for (; i < mid1; i++) s3.offerFirst(s2.pollFirst()); // special use of for loop in order to code this in one line
+        for (; j < mid2; j++) s3.offerFirst(s1.pollFirst());
 
         for (int k = 0; k < len; k++) s1.offerFirst(s3.pollFirst());
     }
@@ -49,7 +52,7 @@ public class SortNumbersInThreeStacks {
     public static void main(String[] args) {
         SortNumbersInThreeStacks sn3 = new SortNumbersInThreeStacks();
 
-        int[] res = new int[]{3, 7, 5, 1, 9, 8, 6, 2, 4};
+        int[] res = new int[]{3, 8, 4, 5, 1, 7, 2, 6};
         LinkedList<Integer> s1 = new LinkedList<>();
 
         for (int v : res)
