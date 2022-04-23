fd3f90be4b1e5633a5a79fd99617fec1bc9bc90d
Weilong <byegates@gmail.com>
2022-04-23 00:01:17 -0400

    Improve TC

--- PackingUpTheSwags.java
+++ PackingUpTheSwags.java
@@ -14,26 +14,21 @@
 
     Analysis:
     1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-    1 2 3 1 2 2 3 2 1  2  3  3  2  3  4  1  2  2  3  2
+    1 2 3 1 2 3 4 2 1  2  3  3  2  3  4  1  2  2  3  2
  */
 
 import java.util.Arrays;
 
-public class PackingUpTheSwags { // TC: O(n), SC: O(n)
+public class PackingUpTheSwags { // TC: O(n^1.5), SC: O(n)
     public int minBoxes(int n) {
         int[] M = new int[n + 1];
         Arrays.fill(M, Integer.MAX_VALUE);
-
-        for (int i = 1; ; i++) {
-            int sq = i * i;
-            if (sq > n) break;
-            M[sq] = 1;
-            if (sq == n) return 1;
-        }
+        M[0] = 0;
+        M[1] = 1;
 
         for (int i = 2; i <= n; i++)
-            for (int j = 1; j < i; j++)
-                M[i] = Math.min(M[i], M[j] + M[i - j]);
+            for (int j = 1; j * j <= i; j++)
+                M[i] = Math.min(M[i], 1 + M[i - j * j]);
 
         return M[n];
     }
