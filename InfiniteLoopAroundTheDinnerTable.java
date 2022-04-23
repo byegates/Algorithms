32cf5d87139d91f0ecd59d7695e97bd04d541ed3
Weilong <byegates@gmail.com>
2022-04-23 03:14:00 -0400

    use String[] instead of List<String> more test cases

--- InfiniteLoopAroundTheDinnerTable.java
+++ InfiniteLoopAroundTheDinnerTable.java
@@ -22,73 +22,74 @@ import java.util.Collections;
 import java.util.List;
 
 public class InfiniteLoopAroundTheDinnerTable {
-    public boolean isInfiniteLoop(List<String> list) {
-        System.out.printf("%s : ", list);
-        return dfs(0, list);
+    public boolean isInfiniteLoop(String[] names) {
+        System.out.printf("%s : ", Arrays.toString(names));
+        return dfs(0, names);
     }
 
-    private boolean dfs(int idx, List<String> list) {
-        if (idx == list.size() - 1)
-            return false;
+    private boolean dfs(int idx, String[] names) {
+        if (idx == names.length)
+            return valid(names, idx - 1, 0);
 
-        if (isInfinite(list)) return true;
-
-        for (int i = idx; i < list.size(); i++) {
-            if (i != idx) swap(list, i, idx);
-            if (dfs(idx + 1, list)) return true;
-            if (i != idx) swap(list, i, idx);
+        for (int i = idx; i < names.length; i++) {
+            if (!valid(names, idx - 1, i)) continue;
+            if (i != idx) swap(names, i, idx);
+            if (dfs(idx + 1, names)) return true;
+            if (i != idx) swap(names, i, idx);
         }
 
         return false;
     }
 
-    private void swap(List<String> list, int i, int j) {
-        String tmp = list.get(i);
-        list.set(i, list.get(j));
-        list.set(j, tmp);
+    private void swap(String[] names, int i, int j) {
+        String tmp = names[i];
+        names[i] = names[j];
+        names[j] = tmp;
     }
 
-    public boolean isInfinite(List<String> list) {
-        int n = list.size();
-        String cur = list.get(n - 1);
-        for (int i = 0; i < n; i++) {
-            String next = list.get(i);
-            if (cur.charAt(cur.length() - 1) != next.charAt(0))
-                return false;
-            cur = next;
-        }
-        return true;
+    public boolean valid(String[] names, int i, int j) {
+        if (i < 0) return true;
+        return names[i].charAt(names[i].length() - 1) == names[j].charAt(0);
     }
 
     public static void main(String[] args) {
         InfiniteLoopAroundTheDinnerTable il = new InfiniteLoopAroundTheDinnerTable();
         List<String> l1 = new ArrayList<>(Arrays.asList("ALICE", "CHARLES", "ERIC", "SOPHIA"));
-        List<String> l2 = new ArrayList<>(Arrays.asList("ALICE", "ERIC", "CHARLES", "SOPHIA"));
+        List<String> l2 = new ArrayList<>(Arrays.asList("ALICE", "XRIC", "CHARLES", "SOPHIA"));
 //        System.out.println(il.isInfinite(l1));
 //        System.out.println(il.isInfinite(l2));
-        System.out.println(il.isInfiniteLoop(l1)); // true
+        System.out.println(il.isInfiniteLoop(l1.toArray(new String[0]))); // true
+        System.out.println(il.isInfiniteLoop(l2.toArray(new String[0]))); // false
         Collections.shuffle(l1);
-        System.out.println(il.isInfiniteLoop(l1)); // true
+        System.out.println(il.isInfiniteLoop(l1.toArray(new String[0]))); // true
         l1.set(0, "XXX");
-        System.out.println(il.isInfiniteLoop(l1)); // false
+        System.out.println(il.isInfiniteLoop(l1.toArray(new String[0]))); // false
 
         List<String> l3 = new ArrayList<>(Arrays.asList("AxB", "BxC", "CxD", "DxE", "ExF", "FxG", "GxA"));
-        System.out.println(il.isInfiniteLoop(l3)); // true
+        System.out.println(il.isInfiniteLoop(l3.toArray(new String[0]))); // true
         Collections.shuffle(l3);
-        System.out.println(il.isInfiniteLoop(l3)); // true
+        System.out.println(il.isInfiniteLoop(l3.toArray(new String[0]))); // true
         Collections.shuffle(l3);
-        System.out.println(il.isInfiniteLoop(l3)); // true
+        System.out.println(il.isInfiniteLoop(l3.toArray(new String[0]))); // true
 
         List<String> l5 = new ArrayList<>(Arrays.asList("AB", "BA", "A", "AC", "CA", "A", "A"));
-        System.out.println(il.isInfiniteLoop(l5));
+        System.out.println(il.isInfiniteLoop(l5.toArray(new String[0])));
         Collections.shuffle(l5);
-        System.out.println(il.isInfiniteLoop(l5));
+        System.out.println(il.isInfiniteLoop(l5.toArray(new String[0])));
 
         List<String> l4 = new ArrayList<>(Arrays.asList("AxB", "BxC", "CxD", "DxE", "ExF", "FxG", "GxH", "HxI", "IxJ", "JxK", "KxL", "LxM", "MxN", "NxO", "OxP", "PxQ", "QxR", "RxS", "SxT", "TxU", "UxV", "VxW", "WxX", "XxY", "YxZ", "ZxA"));
-        System.out.println(il.isInfiniteLoop(l4)); // true
+        System.out.println(il.isInfiniteLoop(l4.toArray(new String[0]))); // true
         Collections.shuffle(l4);
-        System.out.println(il.isInfiniteLoop(l4)); // too slow, can't solve
-
+        System.out.println(il.isInfiniteLoop(l4.toArray(new String[0]))); // true
+        Collections.shuffle(l4);
+        System.out.println(il.isInfiniteLoop(l4.toArray(new String[0]))); // true
+        Collections.shuffle(l4);
+        System.out.println(il.isInfiniteLoop(l4.toArray(new String[0]))); // true
+        Collections.shuffle(l4);
+        System.out.println(il.isInfiniteLoop(l4.toArray(new String[0]))); // true
+        l4.set(0, "XXX");
+        l4.set(5, "YYY");
+        System.out.println(il.isInfiniteLoop(l4.toArray(new String[0]))); // false
     }
 }
 
