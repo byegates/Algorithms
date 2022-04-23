115534653d733366ab8897944c1c60e98279b650
Weilong <byegates@gmail.com>
2022-04-23 06:43:35 -0400

    Use boolean matrix instead of set

--- WordSearch.java
+++ WordSearch.java
@@ -15,54 +15,46 @@
         Word = “ABCB”      return false
 */
 
-
-import java.util.*;
-
 public class WordSearch {
-    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}, };
-    int rows, cols;
-    public boolean isWord(char[][] board, String s) { // TC: O(m*n*4^len), sc: O(m*n+l)
-        if ((rows = board.length) == 0 || (cols = board[0].length) == 0)
-            return false;
+    public final int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, };
+    private int rows, cols;
+    public boolean isWord(char[][] board, String s) {
+        if ((rows = board.length) == 0 || (cols = board[0].length) == 0) return false;
 
-        char[] word = s.toCharArray();
-        Set<Integer> visited = new HashSet<>();
-
+        boolean[][] visited = new boolean[rows][cols];
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < cols; j++)
-                if (dfs(0, board, word, i, j, visited))
-                    return true;
+                if (dfs(0, s.toCharArray(), i, j, board, visited)) return true;
 
         return false;
     }
 
-    private boolean dfs(int idx, char[][] board, char[] word, int i, int j, Set<Integer> visited) {
-        if (idx == word.length)
-            return true;
-
-        int encode = i * cols + j;
-        if (i < 0 || j < 0 || i >= rows || j >= cols || visited.contains(encode) || board[i][j] != word[idx])
+    private boolean dfs(int idx, char[] a, int i, int j, char[][] board, boolean[][] visited) {
+        if (idx == a.length) return true;
+        if (i < 0 || j < 0 || i >= rows || j >= cols || visited[i][j] || board[i][j] != a[idx])
             return false;
-        
-        visited.add(encode);
-        for (int[] direction : directions) {
-            if (dfs(idx + 1, board, word, i + direction[0], j + direction[1], visited))
-                return true;
-        }
-        visited.remove(encode);
-        
+
+        visited[i][j] = true;
+        for (int[] dir : dirs)
+            if (dfs(idx + 1, a, i + dir[0], j + dir[1], board, visited)) return true;
+        visited[i][j] = false;
+
         return false;
     }
 
     public static void main(String[] args) {
         WordSearch ws = new WordSearch();
+        char[][] b0 = new char[][] {
+                {'A'},
+        };
+        System.out.println(ws.isWord(b0, "A")); // true
         char[][] b1 = new char[][] {
                 {'A','B','C','E'},
                 {'S','F','C','S'},
                 {'A','D','E','E'}
         };
-        System.out.println(ws.isWord(b1,"ABCCED"));
-        System.out.println(ws.isWord(b1,"SEE"));
-        System.out.println(ws.isWord(b1,"ABCB"));
+        System.out.println(ws.isWord(b1,"ABCCED")); // true
+        System.out.println(ws.isWord(b1,"SEE")); // true
+        System.out.println(ws.isWord(b1,"ABCB")); // false
     }
 }
