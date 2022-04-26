db0f9ba314b65c2b6a3568d4f32da90f8bf24b54
Weilong <byegates@gmail.com>
2022-04-25 20:55:42 -0400

    Take width as second parameter for more/better control over formatting. Print 1d array as well

--- util/Matrix.java
+++ util/Matrix.java
@@ -1,16 +1,22 @@
 package util;
 
 public class Matrix {
-    public static String toString(int[][] mx) {
+    public static String toString(int[][] mx, int width) {
+        int rows = mx.length;
         StringBuilder sb = new StringBuilder();
-        for (int[] row : mx) {
+        for (int i = 0; i < rows; i++) {
+            int[] row = mx[i];
             sb.append('[');
             for (int j = 0; j < mx[0].length; j++) {
-                sb.append(String.format("%3s", row[j]));
+                sb.append(String.format("%" + width + "s", row[j]));
                 if (j != mx[0].length - 1) sb.append(", ");
             }
-            sb.append("]\n");
+            sb.append(i == rows - 1 ? "]" : "]\n");
         }
         return sb.toString();
+    }
+
+    public static String toString(int[] a, int width) {
+        return toString(new int[][]{a}, width);
     }
 }
