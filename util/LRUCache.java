b1cd4dbd3d713123f5e9ab493ee6368862b406c2
Weilong <byegates@gmail.com>
2022-04-27 09:47:50 -0400

    better naming and more tests

--- util/LRUCache.java
+++ util/LRUCache.java
@@ -5,14 +5,12 @@ import java.util.Map;
 
 public class LRUCache<K, V> {
     static class Node<K, V> {
-        Node<K, V> next;
-        Node<K, V> prev;
+        Node<K, V> next, prev;
         K key;
         V value;
 
         Node(K key, V value) {
-            this.key = key;
-            this.value = value;
+            update(key, value);
         }
 
         Node<K, V> update(K key, V value) {
@@ -23,30 +21,31 @@ public class LRUCache<K, V> {
     }
 
     private final int limit;
-    private Node<K, V> head;
-    private Node<K, V> tail;
+    private Node<K, V> head, tail;
     private final Map<K, Node<K, V>> map;
 
     // limit is the max capacity of the cache
     public LRUCache(int limit) {
         this.limit = limit;
-        this.map = new HashMap<>();
+        map = new HashMap<>();
+    }
+
+    public boolean isFull() {
+        return map.size() >= limit;
     }
 
     public void set(K key, V value) {
         Node<K, V> node = map.get(key);
-        if (node != null) {
-            node.value = value;
-            remove(node);
-        } else if (map.size() < limit) node = new Node<>(key, value);
-        else node = remove(tail).update(key, value);
-        append(node);
+        if (node == null && !isFull()) insert(new Node<>(key, value));
+        else {
+            if (node == null) node = tail;
+            insert(remove(node).update(key, value));
+        }
     }
 
     public V get(K key) {
         Node<K, V> node = map.get(key);
-        if (node == null) return null;
-        return append(remove(node)).value;
+        return node == null ? null : insert(remove(node)).value;
     }
 
     private Node<K, V> remove(Node<K, V> node) {
@@ -59,7 +58,7 @@ public class LRUCache<K, V> {
         return node;
     }
 
-    private Node<K, V> append(Node<K, V> node) {
+    private Node<K, V> insert(Node<K, V> node) {
         map.put(node.key, node);
         if (head == null) head = tail = node;
         else {
@@ -90,7 +89,18 @@ public class LRUCache<K, V> {
         for (int i = 0; i < arr.length; i++) {
             char c = arr[i];
             lru.set(c, c);
-            System.out.printf("%s: %s: %s\n", lru.toString().equals(results[i]), c, lru);
+            System.out.printf("%5s: %s: %s\n", lru.toString().equals(results[i]), c, lru);
+        }
+        String[] commands = new String[]{"LRUCache","put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"};
+        int[][] args2 = new int[][]{{10},{10,13},{3,17},{6,11},{10,5},{9,10},{13},{2,19},{2},{3},{5,25},{8},{9,22},{5,5},{1,30},{11},{9,12},{7},{5},{8},{9},{4,30},{9,3},{9},{10},{10},{6,14},{3,1},{3},{10,11},{8},{2,14},{1},{5},{4},{11,4},{12,24},{5,18},{13},{7,23},{8},{12},{3,27},{2,12},{5},{2,9},{13,4},{8,18},{1,7},{6},{9,29},{8,21},{5},{6,30},{1,12},{10},{4,15},{7,22},{11,26},{8,17},{9,29},{5},{3,4},{11,30},{12},{4,29},{3},{9},{6},{3,4},{1},{10},{3,29},{10,28},{1,20},{11,13},{3},{3,12},{3,8},{10,9},{3,26},{8},{7},{5},{13,17},{2,27},{11,15},{12},{9,19},{2,15},{3,16},{1},{12,17},{9,1},{6,19},{4},{5},{5},{8,1},{11,7},{5,2},{9,28},{1},{2,2},{7,4},{4,22},{7,24},{9,26},{13,28},{11,26}};
+        LRUCache<Integer, Integer> lru2 = new LRUCache<>(args2[0][0]);
+        for (int i = 1; i < commands.length; i++) {
+            String command = commands[i];
+            int[] arg = args2[i];
+            if (command.equals("put")) lru2.set(arg[0], arg[1]);
+            else if (command.equals("get")) lru2.get(arg[0]);
+            else System.out.println("Wong command: " + command);
+            System.out.println(lru2);
         }
     }
 }
