7ad18d27eba12f718254ee1ef09f1f975b7f56ed
Weilong <byegates@gmail.com>
2022-04-27 08:40:52 -0400

    Add test cases, toString method for easier test, simplified code

--- util/LRUCache.java
+++ util/LRUCache.java
@@ -15,17 +15,19 @@ public class LRUCache<K, V> {
             this.value = value;
         }
 
-        void update(K key, V value) {
+        Node<K, V> update(K key, V value) {
             this.key = key;
             this.value = value;
+            return this;
         }
     }
 
     private final int limit;
     private Node<K, V> head;
     private Node<K, V> tail;
-    private Map<K, Node<K, V>> map;
+    private final Map<K, Node<K, V>> map;
 
+    // limit is the max capacity of the cache
     public LRUCache(int limit) {
         this.limit = limit;
         this.map = new HashMap<>();
@@ -37,20 +39,14 @@ public class LRUCache<K, V> {
             node.value = value;
             remove(node);
         } else if (map.size() < limit) node = new Node<>(key, value);
-        else {
-            node = tail;
-            remove(tail);
-            node.update(key, value);
-        }
+        else node = remove(tail).update(key, value);
         append(node);
     }
 
     public V get(K key) {
         Node<K, V> node = map.get(key);
         if (node == null) return null;
-        remove(node);
-        append(node);
-        return node.value;
+        return append(remove(node)).value;
     }
 
     private Node<K, V> remove(Node<K, V> node) {
@@ -72,5 +68,29 @@ public class LRUCache<K, V> {
             head = node;
         }
         return node;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node<K, V> cur = tail;
+        while (cur != null) {
+            sb.append(cur.key);
+            if (cur.prev != null) sb.append(", ");
+            cur = cur.prev;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LRUCache<Character, Character> lru = new LRUCache<>(3);
+        char[] arr = new char[]{'a', 'b', 'c', 'b', 'a', 'd', 'c', 'd', 'a', 'e'};
+        String[] results = new String[] {"[a]", "[a, b]", "[a, b, c]", "[a, c, b]", "[c, b, a]", "[b, a, d]", "[a, d, c]", "[a, c, d]", "[c, d, a]", "[d, a, e]"};
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            lru.set(c, c);
+            System.out.printf("%s: %s: %s\n", lru.toString().equals(results[i]), c, lru);
+        }
     }
 }
