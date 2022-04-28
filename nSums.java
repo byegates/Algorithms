94e9b7a16d0e307d6594a6f3ce5f2f93463ee910
Weilong <byegates@gmail.com>
2022-04-28 02:52:29 -0400

    Add TC and SC for 2 sum closest Add new problem: 2 sum in 2 arrays

--- nSums.java
+++ nSums.java
@@ -9,6 +9,13 @@ public class nSums {
     The given array is not null and has length of at least 2
     Examples
     A = {1, 4, 7, 13}, target = 7, closest pair is 1 + 7 = 8, return [1, 7].
+
+    Analysis
+    TC: O(nlogn) dominated by sort
+    SC: O(log(n)) by primitive sort(something like quick sort)
+    Without considering sort:
+    TC: O(n)
+    SC: O(1)
      */
     public List<Integer> closest(int[] a, int T) {
         List<Integer> res = new ArrayList<>();
@@ -28,6 +35,38 @@ public class nSums {
 
         return res;
     }
+
+    /*
+    Given two arrays A and B, determine whether there exists a pair of elements, one drawn from each array,
+    that sums to the given target number.
+    Assumptions
+    The two given arrays are not null and have length of at least 1
+    Examples
+    A = {3, 1, 5}, B = {2, 8}, target = 7, return true(pick 5 from A and pick 2 from B)
+    A = {1, 3, 5}, B = {2, 8}, target = 6, return false
+
+    Analysis:
+    assume m is the smaller length of a and b, and n is the larger
+    TC: O(m + n)
+    SC: O(m)
+     */
+    public boolean existSum(int[] a, int[] b, int T) {
+        swapArray(a, b);
+        Set<Integer> set = new HashSet<>();
+        for (int x : a) set.add(x);
+        for (int x : b)
+            if (set.contains( T - x)) return true;
+        return false;
+    }
+
+    private void swapArray(int[] a, int[] b) {
+        if (a.length > b.length) { // swap and make sure 'a' is always the smaller size array
+            int[] tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+
 
     public static void main(String[] args) {
         nSums ns = new nSums();
