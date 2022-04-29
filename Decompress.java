ab98886f02db3604fe2ab1850f6b4442d65d900c
Weilong <byegates@gmail.com>
2022-04-29 08:51:00 -0400

    Add support to decompress single char encoding not followed by number 1, like “acb2c4” → “acbbcccc”

--- Decompress.java
+++ Decompress.java
@@ -1,27 +1,37 @@
-//Given a string in compressed form, decompress it to the original string. The adjacent repeated characters in the original string are compressed to have the character followed by the number of repeated occurrences.
-//
-//        Assumptions
-//
-//        The string is not null
-//
-//        The characters used in the original string are guaranteed to be ‘a’ - ‘z’
-//
-//        There are no adjacently repeated characters with length > 9
-//
-//        Examples
-//
-//        “a1c0b2c4” → “abbcccc”
+/*
+        175. Decompress String II
+        run decompress(String s, true)
+        Given a string in compressed form, decompress it to the original string.
+        The adjacent repeated characters in the original string are compressed to have the character followed by the number of repeated occurrences.
+
+        Assumptions
+        The string is not null
+        The characters used in the original string are guaranteed to be ‘a’ - ‘z’
+        There are no adjacently repeated characters with length > 9
+        Examples
+        “a1c0b2c4” → “abbcccc”
+
+        174. Decompress String I
+        “acb2c4” → “acbbcccc”
+        "ap2lec3n" → "applecccn"
+        run decompress(String s, false)
+*/
 
 public class Decompress {
-    public static String decompress(String s) {
-        return decompress(s.toCharArray());
+    public static String decompress(String s, boolean readOne) {
+        return decompress(s.toCharArray(), readOne);
     }
 
-    public static String decompress(char[] A) { // // TC: O(n), SC: O(1)
+    public static String decompress(char[] A, boolean readOne) { // // TC: O(n), SC: O(1)
         int newLen = 0, write = 0;
         for (int curCharIdx = 0, read = 0;read < A.length;) {
-            if (!Character.isDigit(A[read])) curCharIdx = read++;
-            else {
+            if (!Character.isDigit(A[read])) {
+                curCharIdx = read++;
+                if (!readOne && (read == A.length || !Character.isDigit(A[read]))) {
+                    A[write++] = A[curCharIdx];
+                    newLen++;
+                }
+            } else {
                 int[] temp = new int[2];
                 readDigits(A, read, temp);
                 int curCharCount = temp[0];
@@ -69,15 +79,16 @@ public class Decompress {
 
     private static void readDigits(char[] A, int read, int[] temp) {
         int curCharCount = 0;
-        while (read < A.length && Character.isDigit(A[read])) curCharCount = curCharCount * 10 + (A[read++] - '0');
+        while (read < A.length && Character.isDigit(A[read]))
+            curCharCount = curCharCount * 10 + (A[read++] - '0');
         temp[0] = curCharCount;
         temp[1] = read;
     }
 
     public static void main(String[] args) {
-        System.out.println(decompress("a1c0b2c4"));
-        System.out.println(decompress("a13b21c0d2e11f13"));
-        System.out.println(decompress("e4d3c2b21a0"));
+        System.out.println(decompress("a1c0b2c4", true));
+        System.out.println(decompress("a13b21c0d2e11f13", true));
+        System.out.println(decompress("e4d3c2b21a0", true));
         // quick test of readDigits and readDigitsReversed
         int[] temp = new int[2];
         char[] testCharArr = new char[]{'1', '2' ,'3', '4'};
@@ -85,6 +96,7 @@ public class Decompress {
         System.out.printf("index after read: %2d, count read: %d\n", temp[1], temp[0]);
         readDigitsReversed(testCharArr, 3, temp);
         System.out.printf("index after read: %2d, count read: %d\n", temp[1], temp[0]);
-        System.out.println(decompress("ap2lec3n"));
+        System.out.println(decompress("ap2lec3n", true));
+        System.out.println(decompress("ap2lec3n", false));
     }
 }
