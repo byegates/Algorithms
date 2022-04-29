087ff41223c9d0e3f28193b999f0ddd1dcabc3f3
Weilong <byegates@gmail.com>
2022-04-29 08:52:19 -0400

    Init Version String de-dup 1: s1 Array de-dup 1 - 4: a1 - a4

---
+++ DeDup.java
@@ -0,0 +1,126 @@
+import java.util.Arrays;
+
+public class DeDup {
+    /*
+    79. String DeDup 1
+    Remove adjacent, repeated characters in a given string, leaving only one character for each group of such characters.
+    Assumptions
+    Try to do it in place.
+    Examples
+    “aaaabbbc” is transferred to “abc”
+    Corner Cases
+    If the given string is null, returning null or an empty string are both valid.
+     */
+    public String s1(String s) { // TC: O(n), SC: O(1) excluding String to Array switch, O(n) including that
+        if (s == null || s.length() <= 1) return s;
+        return deDup(s.toCharArray());
+    }
+
+    private String deDup(char[] a) {
+        int keep = 1;
+
+        for (int read = 1; read < a.length; read++)
+            if (a[read] != a[keep - 1])
+                a[keep++] = a[read];
+
+        return new String(a, 0, keep);
+    }
+
+    /*
+    115. Array Deduplication I
+    Given a sorted integer array, remove duplicate elements.
+    For each group of elements with the same value keep only one of them.
+    Do this in-place, using the left side of the original array and maintain the relative order of the elements of the array.
+    Return the array after deduplication.
+    Assumptions
+    The array is not null
+    Examples
+    {1, 2, 2, 3, 3, 3} → {1, 2, 3}
+     */
+    public int[] a1(int[] a) { // TC: O(n), SC: O(1)
+        if (a == null || a.length < 2) return a;
+        int keep = 1;
+        for (int read = 1; read < a.length; read++)
+            if (a[read] != a[keep - 1])
+                a[keep++] = a[read];
+        return Arrays.copyOf(a, keep);
+    }
+
+    /*
+    116. Array Deduplication II
+    Given a sorted integer array, remove duplicate elements. For each group of elements with the same value keep at most two of them. Do this in-place, using the left side of the original array and maintain the relative order of the elements of the array. Return the array after deduplication.
+    Assumptions
+    The given array is not null
+    Examples
+    {1, 2, 2, 3, 3, 3} → {1, 2, 2, 3, 3}
+     */
+    public int[] a2(int[] a) { // TC: O(n), SC: O(1)
+        if (a == null || a.length < 3) return a;
+        int keep = 2;
+        for (int read = 2; read < a.length; read++)
+            if (a[read] != a[keep - 2])
+                a[keep++] = a[read];
+        return Arrays.copyOf(a, keep);
+    }
+
+    /*
+    117. Array Deduplication III
+    Given a sorted integer array, remove duplicate elements.
+    For each group of elements with the same value do not keep any of them.
+    Do this in-place, using the left side of the original array and maintain the relative order of the elements of the array.
+    Return the array after deduplication.
+    Assumptions
+    The given array is not null
+    Examples
+    {1, 2, 2, 3, 3, 3} → {1}
+     */
+    public int[] a3(int[] a) {
+        if (a == null || a.length < 2) return a;
+        int keep = 0;
+        for (int read = 0; read < a.length; ) {
+            int start = read++;
+            while (read < a.length && a[read] == a[start]) read++;
+            if (read - start == 1) a[keep++] = a[start];
+        }
+        return Arrays.copyOf(a, keep);
+    }
+    /*
+    118. Array Deduplication IV
+    Given an unsorted integer array, remove adjacent duplicate elements repeatedly, from left to right.
+    For each group of elements with the same value do not keep any of them.
+    Do this in-place, using the left side of the original array. Return the array after deduplication.
+    Assumptions
+    The given array is not null
+    Examples
+    {1, 2, 3, 3, 3, 2, 2} → {1, 2, 2, 2} → {1}, return {1}
+     */
+    public int[] a4(int[] a) {
+        if (a == null || a.length < 2) return a;
+        int top = -1;
+        for (int read = 0; read < a.length;)
+            if (top == -1 || a[top] != a[read]) a[++top] = a[read++];
+            else {
+                while (read < a.length && a[read] == a[top]) read++;
+                top--;
+            }
+        return Arrays.copyOf(a, top + 1);
+    }
+
+    public static void main(String[] args) {
+        DeDup dedup = new DeDup();
+        System.out.println(dedup.s1("aaaabbbc").equals("abc"));
+        System.out.println(dedup.s1("").equals(""));
+        System.out.println(dedup.s1(null) == null);
+
+        System.out.println(Arrays.equals(dedup.a1(new int[]{1, 2, 2, 3, 3, 3}), new int[]{1, 2, 3}));
+        System.out.println(Arrays.equals(dedup.a1(new int[]{1, 2, 3, 4, 4, 5}), new int[]{1, 2, 3, 4, 5}));
+        System.out.println(Arrays.equals(dedup.a2(new int[]{1, 1, 2, 3, 3, 3, 4, 5, 5, 5}), new int[]{1, 1, 2, 3, 3, 4, 5, 5}));
+        System.out.println(Arrays.equals(dedup.a3(new int[]{1, 2, 2, 3, 3, 3}), new int[]{1}));
+        System.out.println(Arrays.equals(dedup.a3(new int[]{1, 1, 2, 3, 3, 3, 4, 5, 5, 5}), new int[]{2, 4}));
+        System.out.println(Arrays.equals(dedup.a4(new int[]{1, 2, 3, 3, 3, 2, 2}), new int[]{1}));
+        System.out.println(Arrays.equals(dedup.a4(new int[]{1, 2, 3, 3, 3, 2, 2}), new int[]{1}));
+        System.out.println(Arrays.equals(dedup.a4(new int[]{1, 1, 2, 3, 3, 3, 2, 1, 6}), new int[]{1, 6}));
+        System.out.println(Arrays.toString(dedup.a3(new int[]{1, 2, 2, 3, 3, 3})));
+    }
+
+}
