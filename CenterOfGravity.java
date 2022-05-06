95188082f73ac568961523998170cb52a5fe71b6
Weilong <byegates@gmail.com>
2022-05-06 09:15:40 -0400

    Shift the sequence of test cases Add problem description

--- CenterOfGravity.java
+++ CenterOfGravity.java
@@ -1,4 +1,11 @@
 /*
+Problem Description:
+Give an array of positive integers, return the center of gravity.
+Center of gravity is the position of 2 adjacent elements, that the sum of the left side is closest to the sum of the right side.
+
+{3, 4,| 2, 2, 2}
+    i   j
+
 Left of Center: 1, left vs right sum: 11 vs  8, Array: [4, 7, 0, 3, 4, 1]
 Left of Center: 1, left vs right sum: 11 vs  8, Array: [4, 7, 0, 3, 4, 1]
 Left of Center: 3, left vs right sum: 17 vs 15, Array: [5, 1, 7, 4, 8, 3, 4]
@@ -13,32 +20,32 @@ Left of Center: 1, left vs right sum:  7 vs  5, Array: [2, 5, 5]
 Left of Center: 1, left vs right sum:  7 vs  5, Array: [2, 5, 5]
 Left of Center: 0, left vs right sum:  9 vs 15, Array: [9, 9, 6]
 Left of Center: 0, left vs right sum:  9 vs 15, Array: [9, 9, 6]
-Left of Center: 2, left vs right sum: 18 vs 18, Array: [3, 6, 9, 2, 8, 3, 5]
-Left of Center: 2, left vs right sum: 18 vs 18, Array: [3, 6, 9, 2, 8, 3, 5]
 Left of Center: 3, left vs right sum: 17 vs 13, Array: [1, 8, 3, 5, 9, 4]
 Left of Center: 3, left vs right sum: 17 vs 13, Array: [1, 8, 3, 5, 9, 4]
 Left of Center: 0, left vs right sum:  8 vs 13, Array: [8, 7, 1, 3, 2, 0]
 Left of Center: 0, left vs right sum:  8 vs 13, Array: [8, 7, 1, 3, 2, 0]
-Left of Center: 4, left vs right sum: 22 vs 17, Array: [5, 1, 5, 3, 8, 4, 5, 1, 7]
-Left of Center: 4, left vs right sum: 22 vs 17, Array: [5, 1, 5, 3, 8, 4, 5, 1, 7]
 Left of Center: 5, left vs right sum: 24 vs 23, Array: [4, 9, 1, 7, 0, 3, 7, 6, 1, 9]
 Left of Center: 5, left vs right sum: 24 vs 23, Array: [4, 9, 1, 7, 0, 3, 7, 6, 1, 9]
-Left of Center: 1, left vs right sum:  9 vs  9, Array: [8, 1, 9]
-Left of Center: 1, left vs right sum:  9 vs  9, Array: [8, 1, 9]
+Left of Center: 3, left vs right sum: 18 vs 15, Array: [3, 0, 6, 9, 1, 2, 9, 3]
+Left of Center: 3, left vs right sum: 18 vs 15, Array: [3, 0, 6, 9, 1, 2, 9, 3]
+Left of Center: 4, left vs right sum: 21 vs 19, Array: [3, 4, 2, 6, 6, 9, 6, 4, 0]
+Left of Center: 4, left vs right sum: 21 vs 19, Array: [3, 4, 2, 6, 6, 9, 6, 4, 0]
 Left of Center: 2, left vs right sum: 19 vs 18, Array: [9, 7, 3, 4, 6, 4, 4]
 Left of Center: 2, left vs right sum: 19 vs 18, Array: [9, 7, 3, 4, 6, 4, 4]
 Left of Center: 0, left vs right sum:  8 vs 10, Array: [8, 5, 4, 1, 0]
 Left of Center: 0, left vs right sum:  8 vs 10, Array: [8, 5, 4, 1, 0]
 Left of Center: 3, left vs right sum: 15 vs 16, Array: [1, 5, 8, 1, 9, 0, 0, 2, 0, 5]
 Left of Center: 3, left vs right sum: 15 vs 16, Array: [1, 5, 8, 1, 9, 0, 0, 2, 0, 5]
+Left of Center: 4, left vs right sum: 22 vs 17, Array: [5, 1, 5, 3, 8, 4, 5, 1, 7]
+Left of Center: 4, left vs right sum: 22 vs 17, Array: [5, 1, 5, 3, 8, 4, 5, 1, 7]
+Left of Center: 1, left vs right sum:  9 vs  9, Array: [8, 1, 9]
+Left of Center: 1, left vs right sum:  9 vs  9, Array: [8, 1, 9]
 Left of Center: 2, left vs right sum: 10 vs 10, Array: [0, 8, 2, 6, 4]
 Left of Center: 2, left vs right sum: 10 vs 10, Array: [0, 8, 2, 6, 4]
 Left of Center: 1, left vs right sum: 11 vs 11, Array: [8, 3, 3, 8, 0]
 Left of Center: 1, left vs right sum: 11 vs 11, Array: [8, 3, 3, 8, 0]
-Left of Center: 3, left vs right sum: 18 vs 15, Array: [3, 0, 6, 9, 1, 2, 9, 3]
-Left of Center: 3, left vs right sum: 18 vs 15, Array: [3, 0, 6, 9, 1, 2, 9, 3]
-Left of Center: 4, left vs right sum: 21 vs 19, Array: [3, 4, 2, 6, 6, 9, 6, 4, 0]
-Left of Center: 4, left vs right sum: 21 vs 19, Array: [3, 4, 2, 6, 6, 9, 6, 4, 0]
+Left of Center: 2, left vs right sum: 18 vs 18, Array: [3, 6, 9, 2, 8, 3, 5]
+Left of Center: 2, left vs right sum: 18 vs 18, Array: [3, 6, 9, 2, 8, 3, 5]
 */
 
 import java.util.Arrays;
