e7a1c71b30ab8e9a991b829ee78d0f5f0d573110
Weilong <byegates@gmail.com>
2022-05-06 03:39:55 -0400

    Minor change

--- util/ListNode.java
+++ util/ListNode.java
@@ -225,14 +225,15 @@ public class ListNode {
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast) break;
+            if (slow == fast) {
+                while (head != slow) {
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
         }
-        if (fast == null || fast.next == null) return null;
-        while (head != slow) {
-            head = head.next;
-            slow = slow.next;
-        }
-        return slow;
+        return null;
     }
 
     public static void main(String[] args) {
