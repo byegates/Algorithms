fba3a6422b52fd658eec3d63cdf5fb2aa193a979
Weilong <byegates@gmail.com>
2022-05-06 18:25:18 -0400

    Add comment

--- InsertionSortLinkedList.java
+++ InsertionSortLinkedList.java
@@ -1,7 +1,5 @@
 import util.ListNode;
 
-import java.util.List;
-
 public class InsertionSortLinkedList {
     public ListNode insertionSort(ListNode head) { // TC: O(n^2), SC: O(n)
         if (head == null) return null;
@@ -12,14 +10,26 @@ public class InsertionSortLinkedList {
             ListNode s2 = dummy;
             for (; s2 != s1; s2 = s2.next) {
                 if (s2.next.value > s1.next.value) {
-                    ListNode s3 = s1.next;
-                    s1.next = s3.next;
-                    s3.next = s2.next;
-                    s2.next = s3;
+                    // index 0 1 2 3
+                    // dummy 5 4 3 2
+                    //      s1
+                    //   s2
+                    ListNode temp = s1.next; // temp = 4
+                    s1.next = temp.next; // 5 --> 3
+                    temp.next = s2.next; // 4 --> 5
+                    s2.next = temp; // s2 --> 4
                     break;
+                    // index 0 1 2 3
+                    // dummy 4 5 3 2
+                    //        s1
+                    //   s2
                 }
             }
             if (s2 == s1) s1 = s1.next;
+            // index 0 1 2 3
+            // dummy 2 3 4 5 6
+            //               s1
+            //            s2
         }
 
         return dummy.next;
