5dea371bcf09040aee03bf7be0faba18f472f007
Weilong <byegates@gmail.com>
2022-05-07 22:38:52 -0400

    Better performance

--- XorGreaterThanAndPair.java
+++ XorGreaterThanAndPair.java
@@ -7,9 +7,9 @@ public class XorGreaterThanAndPair {
     public long bigXorSum(List<Integer> list) {
         int[] count = new int[31]; // for any positive integer, the msb index can be 0 to 30 in total 31 possibilities, (max int is 2^31 - 1)
         long res = 0;
-        for (Integer num : list) {
-            int msb = 31 - Integer.numberOfLeadingZeros(num); // msb stands for most significant digit, meaning the left most 1, and here we need its index
-            res += sum(count) - count[msb]; // every round we make a pair with all previous numbers, but exclude numbers with same msb index
+        for (int i = 0; i < list.size(); i++) {
+            int msb = 31 - Integer.numberOfLeadingZeros(list.get(i)); // msb stands for most significant digit, meaning the left most 1, and here we need its index
+            res += i - count[msb]; // every round we make a pair with all previous numbers, but exclude numbers with same msb index
             count[msb]++;
         }
         return res;
