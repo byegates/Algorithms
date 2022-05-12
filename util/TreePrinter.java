eb06c1f8d350d73e5f1f7d75b3c54a6cd78721a5
Weilong <byegates@gmail.com>
2022-05-12 08:35:21 -0400

    Simplify Code

--- util/TreePrinter.java
+++ util/TreePrinter.java
@@ -32,19 +32,17 @@ public class TreePrinter {
         int lvlNodeWidth = (int) Math.pow(2, height - lvl) * width; // the width of each node at each level, it's going to decrease by half per level
 
         for (List<Integer> curLevelKeys : allKeys) {
-            for (int i = 0; i < curLevelKeys.size(); i++) {
-                if (lvl > 1) { // we print '/' and '\' first and starting from 2nd level
-                    String s = i % 2 == 0 ? "/" : "\\";
-                    sb.append(center(s, lvlNodeWidth, i % 2 != 0)); // always rightAlign '\' and leftAlign '/'
-                }
-            }
+            for (int i = 0; i < curLevelKeys.size(); i++)
+                if (lvl > 1) // we print '/' and '\' first and starting from 2nd level
+                    sb.append(center(i % 2 == 0 ? "/" : "\\", lvlNodeWidth, i % 2 != 0)); // always rightAlign '/' and leftAlign '\'
+
             sb.append("\n"); // done printing all the back and forward slashes
 
-            for (Integer key : curLevelKeys) {
-                if (key == null) sb.append(center("x", lvlNodeWidth, true));
-                else sb.append(center(key.toString(), lvlNodeWidth, true));
-            }
+            for (Integer key : curLevelKeys)
+                sb.append(center(key == null ? "x" : key.toString(), lvlNodeWidth, true));
+
             sb.append("\n"); // done print one level of keys
+
             lvlNodeWidth /= 2; // reduce width per node by half per level
             lvl++;
         }
@@ -87,29 +85,32 @@ public class TreePrinter {
         }
     }
 
+    public static void print(TreeNode root) {
+        System.out.println(toString(root));
+    }
+
     public static void main(String[] args) {
         TreeNode root = TreeNode.fromLevelOrder(new Integer[]{5, null, 8, null, 4, 3, 4});
-        BTreePrinter.printNode(root);
-        System.out.println(TreePrinter.toString(root));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {10, 12, 13, 14, 15, 16, 17, null, null, -108})));
+        print(root);
+        print(TreeNode.fromLevelOrder(new Integer[] {10, 12, 13, 14, 15, 16, 17, null, null, -108}));
 
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {1, 2, 3, 4, 5, 6, 7})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {1, 1, 2, 1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {1, 2, -3, 4, 5, 6, 7})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {10, 12, 13, 14, 15, 16, 17})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {11, 11, 12, 11, 12, 13, 14, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 30, 31, 32, 33, 34, 35, 36})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {11, 11, 12, 11, 12, 13, 14, 21, 22, 23, 24, 25, 26, 27, 28, 31, null, 33, 34, null, 36, null, 38, 39, 30, null, 32, 33, null, 35, 36})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {10, 12, -13, 14, 15, 16, 17})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {10, 12, 13, 14, 15, 16, 17, null, null, -108})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {10, 12, 14, null, 16, 17})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {10, 12, 14, null, -108, 17})));
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(new Integer[] {10, 12, 14, null, 16, 17, -18, 27, null, 36, 48, null})));
+        print(TreeNode.fromLevelOrder(new Integer[] {1, 2, 3, 4, 5, 6, 7}));
+        print(TreeNode.fromLevelOrder(new Integer[] {1, 1, 2, 1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6}));
+        print(TreeNode.fromLevelOrder(new Integer[] {1, 2, -3, 4, 5, 6, 7}));
+        print(TreeNode.fromLevelOrder(new Integer[] {10, 12, 13, 14, 15, 16, 17}));
+        print(TreeNode.fromLevelOrder(new Integer[] {11, 11, 12, 11, 12, 13, 14, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 30, 31, 32, 33, 34, 35, 36}));
+        print(TreeNode.fromLevelOrder(new Integer[] {11, 11, 12, 11, 12, 13, 14, 21, 22, 23, 24, 25, 26, 27, 28, 31, null, 33, 34, null, 36, null, 38, 39, 30, null, 32, 33, null, 35, 36}));
+        print(TreeNode.fromLevelOrder(new Integer[] {10, 12, -13, 14, 15, 16, 17}));
+        print(TreeNode.fromLevelOrder(new Integer[] {10, 12, 13, 14, 15, 16, 17, null, null, -108}));
+        print(TreeNode.fromLevelOrder(new Integer[] {10, 12, 14, null, 16, 17}));
+        print(TreeNode.fromLevelOrder(new Integer[] {10, 12, 14, null, -108, 17}));
+        print(TreeNode.fromLevelOrder(new Integer[] {10, 12, 14, null, 16, 17, -18, 27, null, 36, 48, null}));
         Integer[] arr2 = new Integer[] {111, 111, 112, 111, 112, 113, 114, 121, 122, 123, 224, 225, 226, 227, 228, 231, null, 333, 334, null, 336, null, 338, 339, 330, null, 332, 333, null, 335, 336};
         TreeNode root2 = TreeNode.fromLevelOrder(arr2);
         System.out.println(root2);
         // test case with more negative numbers
         Integer[] arr2b = new Integer[arr2.length];
         for (int i = 0; i < arr2.length; i++) arr2b[i] = arr2[i] != null ? i % 2 == 0 ? arr2[i] : -arr2[i] : null;
-        System.out.println(TreePrinter.toString(TreeNode.fromLevelOrder(arr2b)));
+        print(TreeNode.fromLevelOrder(arr2b));
     }
 }
