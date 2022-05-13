c868bab1d8774b7194d83d893d8e05635d1a52d1
Weilong <byegates@gmail.com>
2022-05-13 10:12:16 -0400

    Merge Matrix Class into Utils class

--- util/Utils.java
+++ util/Utils.java
@@ -9,6 +9,21 @@ import static resources.ConsoleColors.RESET;
 
 public class Utils {
     private final static Random rand = new Random();
+
+    public static void main(String[] args) {
+        for (int i = 1; i < Integer.MAX_VALUE/2; i *= 10) {
+            int num = i + i / 10 + i / 100;
+            System.out.printf("%10d: %d\n", num, numOfDigits(num));
+        }
+
+        int[][] mx = new int[][]{{1, 2,}, {3, 4}};
+        System.out.println(Utils.toString(mx));
+        System.out.println(Utils.toString(mx, 3));
+        int[] a = new int[]{1, 2,};
+        System.out.println(Utils.toString(a));
+        System.out.println(Utils.toString(a, 3));
+    }
+
     public static String toString(int e, int radix) {
         StringBuilder sb = new StringBuilder();
         sb.append("0".repeat(Integer.numberOfLeadingZeros(e)));
@@ -120,10 +135,67 @@ public class Utils {
         return list;
     }
 
-    public static void main(String[] args) {
-        for (int i = 1; i < Integer.MAX_VALUE/2; i *= 10) {
-            int num = i + i / 10 + i / 100;
-            System.out.printf("%10d: %d\n", num, numOfDigits(num));
+    public static String toString(int[][] mx, int width) {
+        int rows = mx.length;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < rows; i++) {
+            int[] row = mx[i];
+            sb.append('[');
+            for (int j = 0; j < mx[0].length; j++) {
+                sb.append(String.format("%" + width + "s", row[j]));
+                if (j != mx[0].length - 1) sb.append(", ");
+            }
+            sb.append(i == rows - 1 ? "]" : "]\n");
         }
+        return sb.toString();
+    }
+
+    public static String toStringMD(int[][] mx, int width) {
+        int rows = mx.length, cols = mx[0].length;
+        StringBuilder sb = new StringBuilder();
+        // add header
+        sb.append("| ").append(" ".repeat(width));
+        for (int j = 0; j < cols; j++) sb.append(String.format("| %-" + width + "s", j));
+        sb.append("|\n");
+
+        // add ----- line
+        for (int j = 0; j <= cols; j++) sb.append("|-").append("-".repeat(width));
+        sb.append("|\n");
+
+        // add context
+        for (int i = 0; i < rows; i++) {
+            int[] row = mx[i];
+            sb.append(String.format("| %-" + width + "s|", i));
+            for (int j = 0; j < cols; j++) {
+                sb.append(String.format(" %-" + width + "s", row[j])).append("|");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static String toString(int[] a, int width) {
+        return toString(new int[][]{a}, width);
+    }
+
+    public static String toString(int[] a) {
+        return toString(new int[][]{a}, 1);
+    }
+
+    public static String toString(int[][] mx) {
+        return toString(mx, 1);
+    }
+
+    public static int sum(int[] a) {
+        return sum(new int[][]{a});
+    }
+
+    public static int sum(int[][] mx) {
+        int sum = 0;
+        for (int[] row : mx)
+            for (int x : row)
+                sum += x;
+        return sum;
+
     }
 }
