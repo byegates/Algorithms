8c2da3939b4e37a3cab06800be25e0db8934f037
Weilong <byegates@gmail.com>
2022-06-26 20:43:16 -0500

    more comments

--- Decompress.java
+++ Decompress.java
@@ -29,28 +29,28 @@ public class Decompress {
     public static String decompress(char[] a, boolean fixLen) {
         int write = 0, newLen = 0;
         for (int read = 0; read < a.length; ) {
-            int curCharIdx = read++, count = 0;
-            for (; read < a.length && a[read] >= '0' && a[read] <= '9'; read++)
+            int curCharIdx = read++, count = 0; // curCharIdx to remember the first char of each group
+            for (; read < a.length && a[read] >= '0' && a[read] <= '9'; read++) // get count of each char (if exist)
                 count = a[read] - '0' + count * 10;
             if (!fixLen && count == 0) count = 1; // to accommodate two different type of encoding: ab2 vs a1b2
-            newLen += count;
+            newLen += count; // calculate overall len of new String
             if (read - write >= count) // enough space to decompress in-place, so we do it
                 for (; count > 0; count--) a[write++] = a[curCharIdx];
             else // Not enough space to decompress in-place right away, just copy everything for now
                 while (curCharIdx < read) a[write++] = a[curCharIdx++];
         }
-        if (newLen == write) return new String(a, 0, write);
-        return round2(a, write - 1, newLen, newLen > a.length ? new char[newLen] : a);
+        if (newLen == write) return new String(a, 0, write); // we've done everything in-place, return
+        return round2(a, write - 1, newLen, newLen > a.length ? new char[newLen] : a); // need round2 to write chars we couldn't finish earlier
     }
 
     private static String round2(char[] src, int read, int len, char[] dst) {
         for (int write = len; read >= 0; ) {
             char c = src[read--];
             int count = 0;
-            for (int power = 0 ; c >= '0' && c <= '9'; c = src[read--])
+            for (int power = 0 ; c >= '0' && c <= '9'; c = src[read--]) // get count, formula as below
                 count += (c - '0') * Math.pow(10, power++); // 123: 3 * 10^0 + 2 * 10^1 + 1 * 10^2
-            while (count-- > 1) dst[--write] = c;
-            dst[--write] = c;
+            dst[--write] = c; // write one char no matter what
+            while (count-- > 1) dst[--write] = c; // write additional chars if count is greater than 1
         }
         return new String(dst, 0, len);
     }
