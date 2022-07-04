7ea8d464c411b108337b422e740484514092c6e3
Weilong <byegates@gmail.com>
2022-07-04 17:40:33 -0500

    More comments

--- Translator.java
+++ Translator.java
@@ -5,7 +5,7 @@ public class Translator {
     private final Map<String, String> roots = new HashMap<>();
     private final Map<String, Map<String, String>> dict = new HashMap<>();
     public void add(String lang1, String word1, String lang2, String word2) { // 1 is input, 2 is output
-        init(lang1, word1); init(lang2, word2);
+        init(lang1, word1); init(lang2, word2); // initialize each word for union find
         String root1 = find(word1), root2 = find(word2);
         if (!root1.equals(root2)) {
             roots.put(root2, root1);
@@ -52,13 +52,17 @@ public class Translator {
         tl.add("KR", "안녕하십니까", "JP", "こんにちは");
         tl.add("GR", "Hallo", "JP", "こんにちは");
         tl.add("GR", "Hallo", "EN", "Bonjour");
+
         String[] langs = new String[] {"EN", "FR", "ES", "CN", "KR", "JP", "GR"};
         String[] words = new String[] {"Hello", "Bonjour", "Hola", "你好", "안녕하십니까", "こんにちは", "Hallo"};
+
+        System.out.println("Translate Hola into Different Languages:");
         for (String q : langs) System.out.printf("%s: %s\n", q, tl.get("ES", "Hola", q));
         System.out.println();
         System.out.printf("dict  after Round1 queries: %s\n", tl.getDict());
         System.out.printf("roots after Round1 queries: %s\n", tl.getRoots());
         System.out.println();
+        System.out.println("Translate different Language words into French:");
         for (String s : words) System.out.printf("%s(FR) <-- %s\n", tl.get("ES", s, "FR"), s);
         System.out.println();
         System.out.printf("dict  after Round2 queries: %s\n", tl.getDict());
@@ -69,6 +73,7 @@ public class Translator {
 
 /*
 All prints:
+Translate Hola into Different Languages:
 EN: Hello
 FR: Bonjour
 ES: Hola
@@ -80,6 +85,7 @@ GR: Hallo
 dict  after Round1 queries: {Hallo={JP=こんにちは, KR=안녕하십니까, EN=Hello, GR=Hallo, CN=你好, FR=Bonjour, ES=Hola}}
 roots after Round1 queries: {Hallo=Hallo, こんにちは=你好, Hello=Hallo, 你好=Hallo, Bonjour=Hallo, 안녕하십니까=你好, Hola=Hallo}
 
+Translate different Language words into French:
 Bonjour(FR) <-- Hello
 Bonjour(FR) <-- Bonjour
 Bonjour(FR) <-- Hola
